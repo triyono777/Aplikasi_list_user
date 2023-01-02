58362f06bd572857fa763845245c519cc181ce30
Triyono <yono.tri@gmail.com>
2023-01-02 08:14:04 +0700

    init commit

---
+++ lib/data_contoh/data_profil.dart
@@ -0,0 +1,34 @@
+import 'package:aplikasi_pertama/data_contoh/model_profil.dart';
+
+class DataProfil {
+  List<ModelProfil> listProfil = [
+    ModelProfil(
+        email: 'budi@email.com',
+        follower: '20000',
+        following: '10000',
+        nama: 'budi',
+        noHape: '088991711111',
+        pekerjaan: 'ui ux desainer'),
+    ModelProfil(
+        email: 'alx@email.com',
+        follower: '20000',
+        following: '10000',
+        nama: 'alx',
+        noHape: '088991711111',
+        pekerjaan: 'ui ux desainer'),
+    ModelProfil(
+        email: 'rudi@email.com',
+        follower: '20000',
+        following: '10000',
+        nama: 'rudi',
+        noHape: '088991711111',
+        pekerjaan: 'ui ux desainer'),
+    ModelProfil(
+        email: 'joko@email.com',
+        follower: '20000',
+        following: '10000',
+        nama: 'joko',
+        noHape: '088991711111',
+        pekerjaan: 'ui ux desainer'),
+  ];
+}
---
+++ lib/data_contoh/model_profil.dart
@@ -0,0 +1,17 @@
+class ModelProfil {
+  String? nama;
+  String? pekerjaan;
+  String? follower;
+  String? following;
+  String? email;
+  String? noHape;
+
+  ModelProfil({
+    this.nama,
+    this.pekerjaan,
+    this.follower,
+    this.following,
+    this.email,
+    this.noHape,
+  });
+}
